6d30894d73f8c5c1ef2a9321c9487c0dc92f4c70
lokaimoma <kelvinclark2k77@gmail.com>
2023-01-02 17:30:50 +0100

    Rewrite session struct

--- src/session.rs
+++ src/session.rs
@@ -3,22 +3,9 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Session {
-    pub title: String,
-    /// The first page's url.
-    pub index_url: String,
-    /// The destination for the downloaded files. This path must exist.
-    pub destination_directory: String,
-    pub resources: HashMap<String, Resource>,
-}
-
-pub struct SessionUpdate {
-    pub session_url: String,
-    pub resource_url: String,
-    pub message: Option<String>,
-    pub bytes_written: Option<u64>,
-    pub total_size: Option<u64>,
-}
-
-impl Session {
-    async fn relink_resources() {}
+    pub session_id: String,
+    /// A url string to file destination map of all processed pages
+    pub processed_pages: HashMap<String, String>,
+    /// A url string to file destination map of all processed static resources
+    pub processed_static_files: HashMap<String, String>pub ,
 }
