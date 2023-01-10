d7cb87f41de12f662d45f554d62f6e184f6478c3
lokaimoma <kelvinclark2k77@gmail.com>
2023-01-10 18:45:30 +0100

    Set up tracing subscriber

--- wsclone_cli/src/main.rs
+++ wsclone_cli/src/main.rs
@@ -1,10 +1,18 @@
 use crate::cli::download;
 use clap::Parser;
+use std::path::MAIN_SEPARATOR;
 
 mod cli;
 
 #[tokio::main]
 async fn main() {
+    let f_appender =
+        tracing_appender::rolling::hourly(format!(".{}", MAIN_SEPARATOR), "wsclone.log");
+    let (non_blk, _guard) = tracing_appender::non_blocking(f_appender);
+    tracing_subscriber::fmt()
+        .event_format(tracing_subscriber::fmt::format().pretty())
+        .with_writer(non_blk)
+        .init();
     pretty_env_logger::formatted_builder()
         .filter(Some("wsclone"), log::LevelFilter::Trace)
         .init();
