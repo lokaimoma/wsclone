d4c93aa7f8b78a1785cd0949babea19fb0934440
lokaimoma <kelvinclark2k77@gmail.com>
2023-02-06 23:25:49 +0100

    Create payload trait for serialization and deserialization of command and response types

--- ui/common/src/lib.rs
+++ ui/common/src/lib.rs
@@ -1,4 +1,15 @@
+use serde::{Deserialize, Serialize};
+
 pub mod command;
 mod error;
 pub mod ipc_helpers;
 pub mod response;
+
+pub trait Payload {
+    fn from_str<'a, T>(payload: &'a str) -> error::Result<T>
+    where
+        T: Deserialize<'a>;
+    fn to_bytes<T>(&self) -> error::Result<Vec<u8>>
+    where
+        T: Serialize;
+}
