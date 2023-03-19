73f9a8504c27f65914ff8de795a2b46963b6ae1b
lokaimoma <kelvinclark2k77@gmail.com>
2023-03-19 12:45:26 +0000

    Add command to plugin
    Nothing has been put inside the command yet

--- ui/desktop/src-tauri/src/main.rs
+++ ui/desktop/src-tauri/src/main.rs
@@ -13,7 +13,7 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .plugin(plugin::WSClonePlugin{})
+        .plugin(plugin::WSClonePlugin::new())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- ui/desktop/src-tauri/src/plugin.rs
+++ ui/desktop/src-tauri/src/plugin.rs
@@ -1,10 +1,24 @@
-use tauri::{plugin::Plugin, Manager, Runtime};
+use tauri::{plugin::Plugin, Invoke, Manager, Runtime, State};
 
 use crate::state::AppState;
 
-pub struct WSClonePlugin;
+pub struct WSClonePlugin<R: Runtime> {
+    invoke_handler: Box<dyn Fn(Invoke<R>) + Send + Sync + 'static>,
+}
 
-impl<R: Runtime> Plugin<R> for WSClonePlugin {
+#[tauri::command]
+fn handle_command<'a>(app_state: State<'a, AppState>) {}
+
+impl<R: Runtime> WSClonePlugin<R> {
+    pub fn new() -> Self {
+        Self {
+            invoke_handler: Box::new(tauri::generate_handler![handle_command]),
+        }
+    }
+}
+
+
+impl<R: Runtime> Plugin<R> for WSClonePlugin<R> {
     fn name(&self) -> &'static str {
         "wsclone"
     }
@@ -17,5 +31,9 @@ impl<R: Runtime> Plugin<R> for WSClonePlugin {
         let app_state = AppState::new().unwrap();
         app.manage(app_state);
         Ok(())
+    }
+
+    fn extend_api(&mut self, invoke: Invoke<R>) {
+        (self.invoke_handler)(invoke)
     }
 }
